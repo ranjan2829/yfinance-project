9880b2c78358a0111072dcb5c05da03c6c14250c
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2022-09-26 03:40:17 +0530

    Add files via upload

---
+++ scrap.py
@@ -0,0 +1,19 @@
+# IMPORT ALL THE REQUIRED LIBRARIES
+from bs4 import BeautifulSoup as BS
+import requests as req
+
+url = "https://www.businesstoday.in/latest/economy"
+
+webpage = req.get(url)
+trav = BS(webpage.content, "html.parser")
+M = 1
+for link in trav.find_all('a'):
+	
+	# PASTE THE CLASS TYPE THAT WE GET
+	# FROM THE ABOVE CODE IN THIS AND
+	# SET THE LIMIT GRATER THAN 35
+	if(str(type(link.string)) == "<class 'bs4.element.NavigableString'>"
+	and len(link.string) > 35):
+
+		print(str(M)+".", link.string)
+		M += 1
