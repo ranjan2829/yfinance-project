b8332775cffd4983e6155607ce873377fc23edf2
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2022-09-26 22:47:29 +0530

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -17,3 +17,92 @@ for link in trav.find_all('a'):
 
 		print(str(M)+".", link.string)
 		M += 1
+		
+		
+		
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+# Here you can use markdown language to make your app prettier
+st.write("""
+# Financial App
+""")
+
+
+
+
+
+from datetime import date
+
+# Returns the current local date
+today = date.today()
+
+
+
+d = st.date_input(
+    "Select the Historical data",
+    date(2019, 7, 6))
+st.write('your selected date is :', d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    st.title('Search Medium Story')
+    stock = st.text_input('Enter search words:')
+    
+    # Get stock data
+    get_stock_data = yf.Ticker(stock)
+
+    # Set the time line of your data
+    ticket_df = get_stock_data.history(period='1d', start=f'{d}', end=f'{today}')
+
+    # Show your data in line chart
+    st.line_chart(ticket_df.Close)
+    st.line_chart(ticket_df.Volume)
+
+
+   
+    
+ 
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
