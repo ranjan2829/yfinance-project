704e54b4de0dfccc4cf7284743a06cc9e092f323
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2022-09-26 23:39:42 +0530

    Update sample_project.py

--- sample_project.py
+++ sample_project.py
@@ -85,29 +85,12 @@ def main():
     ticket_df = get_stock_data.history(period='1d', start=f'{d}', end=f'{today}')
 
     # Show your data in line chart
-    st.line_chart(ticket_df.Close)
-    st.line_chart(ticket_df.Volume)
-# IMPORT ALL THE REQUIRED LIBRARIES
-
-
-d = st.date_input(
-    "Select the Historical data",
-    date(2019, 7, 6))
-st.write('your selected date is :', d)
-def main():
-    st.title('Search Medium Story')
-    stock = st.text_input('Enter search words:')
-    
-    # Get stock data
-    get_stock_data = yf.Ticker(stock)
-
-    # Set the time line of your data
-    ticket_df = get_stock_data.history(period='1d', start=f'{d}', end=f'{today}')
-
-    # Show your data in line chart
     st.bar_chart(ticket_df.Close)
     st.bar_chart(ticket_df.Volume)
 # IMPORT ALL THE REQUIRED LIBRARIES
+
+
+
 
 
 
