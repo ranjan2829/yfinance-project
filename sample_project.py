6fd3d76084f9eed09cd485dfcdb560146360d4d4
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2022-09-26 13:04:29 +0530

    Update sample_project.py

--- sample_project.py
+++ sample_project.py
@@ -17,7 +17,16 @@ st.write("""
 def main():
     st.title('Search Medium Story')
     stock = st.text_input('Enter search words:')
+    
+    # Get stock data
+    get_stock_data = yf.Ticker(stock)
 
+    # Set the time line of your data
+    ticket_df = get_stock_data.history(period='1d', start='2021-1-02', end='2021-12-12')
+
+    # Show your data in line chart
+    st.line_chart(ticket_df.Close)
+    st.line_chart(ticket_df.Volume)
 if __name__ == '__main__':
     main()
 
@@ -31,15 +40,4 @@ if __name__ == '__main__':
 
 
 
-# We will use Amazon stocks
 
-
-# Get stock data
-get_stock_data = yf.Ticker(stock)
-
-# Set the time line of your data
-ticket_df = get_stock_data.history(period='1d', start='2021-1-02', end='2021-12-12')
-
-# Show your data in line chart
-st.line_chart(ticket_df.Close)
-st.line_chart(ticket_df.Volume)
