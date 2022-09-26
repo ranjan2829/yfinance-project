dd3507e2175a2a3bfee65068474c5930fdf39020
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2022-09-26 23:21:15 +0530

    Update sample_project.py

--- sample_project.py
+++ sample_project.py
@@ -1,3 +1,36 @@
+
+		
+		
+		
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+# Here you can use markdown language to make your app prettier
+st.write("""
+# Financial App
+""")
+
+
+
+from bs4 import BeautifulSoup as BS
+import requests as req
+
+from datetime import date
+
+# Returns the current local date
+today = date.today()
+
+
+
+d = st.date_input(
+    "Select the Historical data",
+    date(2019, 7, 6))
+st.write('your selected date is :', d)
+
+
+
+
 # IMPORT ALL THE REQUIRED LIBRARIES
 from bs4 import BeautifulSoup as BS
 import requests as req
@@ -15,40 +48,8 @@ for link in trav.find_all('a'):
 	if(str(type(link.string)) == "<class 'bs4.element.NavigableString'>"
 	and len(link.string) > 35):
 
-		print(str(M)+".", link.string)
+		st.write(str(M)+".", link.string)
 		M += 1
-		
-		
-		
-import yfinance as yf
-import streamlit as st
-import pandas as pd
-
-# Here you can use markdown language to make your app prettier
-st.write("""
-# Financial App
-""")
-
-
-
-
-
-from datetime import date
-
-# Returns the current local date
-today = date.today()
-
-
-
-d = st.date_input(
-    "Select the Historical data",
-    date(2019, 7, 6))
-st.write('your selected date is :', d)
-
-
-
-
-
 
 
 
@@ -86,6 +87,8 @@ def main():
     # Show your data in line chart
     st.line_chart(ticket_df.Close)
     st.line_chart(ticket_df.Volume)
+# IMPORT ALL THE REQUIRED LIBRARIES
+
 
 
    
